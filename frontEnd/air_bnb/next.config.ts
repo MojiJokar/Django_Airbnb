ce30c39f73bb6540e719b5173ca4cbca3d586b94
MaxJokar <mmb2020max@gmail.com>
2026-09-25 12:53:57 +0200

    .env.local added to get json in front

--- frontEnd/air_bnb/next.config.ts
+++ frontEnd/air_bnb/next.config.ts
@@ -31,10 +31,42 @@
 // };
 
 // export default nextConfig;
+//----------------------------
+
 //-----------------------------------------------------------
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//     images: {
+//         dangerouslyAllowLocalIP: true,
+
+//         remotePatterns: [
+//             {
+//                 protocol: "http",
+//                 hostname: "localhost",
+//                 port: "8000",
+//                 pathname: "/**",
+//             },
+//             {
+//                 protocol: "http",
+//                 hostname: "64.226.81.32",
+//                 port: "1337",
+//                 pathname: "/**",
+//             },
+//         ],
+//     },
+// };
+
+// export default nextConfig;
+
+//due to some issue of next.js root we made following  changes:
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    turbopack: {
+        root: __dirname,
+    },
+
     images: {
         dangerouslyAllowLocalIP: true,
 
@@ -56,7 +88,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-
 
 
