e8ae8819444e3ebb0862eac01246cae43ce45539
MaxJokar <mmb2020max@gmail.com>
2026-09-26 14:18:45 +0200

    manuscipt Updated

--- backEnd/property/api.py
+++ backEnd/property/api.py
@@ -1,18 +1,91 @@
+# for test
+
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework_simplejwt.tokens import AccessToken
-# from .forms import PropertyForm
-# from .models import Property
+
 from .models import Property, Reservation
-# from .serializers import PropertiesListSerializer
 from .serializers import PropertiesListSerializer, ReservationsListSerializer
-from useraccount.models import User
+
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
-def properties_list(request):
+def property_list(request):
+    properties = Property.objects.all()
+
+    serializer = PropertiesListSerializer(
+        properties,
+        many=True
+    )
+
+    return JsonResponse({
+        'data': serializer.data,
+    })
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_detail(request, id):
+    try:
+        property = Property.objects.get(id=id)
+    except Property.DoesNotExist:
+        return JsonResponse(
+            {'error': 'Property not found'},
+            status=404
+        )
+
+    serializer = PropertiesListSerializer(property)
+
+    return JsonResponse(serializer.data)
+
+
+
+
+
+
+# from django.http import JsonResponse
+
+# from rest_framework.decorators import api_view, authentication_classes, permission_classes
+# from rest_framework_simplejwt.tokens import AccessToken
+# # from .forms import PropertyForm
+# # from .models import Property
+# from .models import Property, Reservation
+# # from .serializers import PropertiesListSerializer
+# from .serializers import PropertiesListSerializer, ReservationsListSerializer
+# from useraccount.models import User
+
+# @api_view(['GET'])
+# @authentication_classes([])
+# @permission_classes([])
+# def properties_list(request):
+
+#     properties = Property.objects.all()
+#     serializer = PropertiesListSerializer(properties, many=True)
+
+#     return JsonResponse({
+#         'data': serializer.data,
+        
+#     })
+
+
+
+# from django.http import JsonResponse
+
+# from rest_framework.decorators import api_view, authentication_classes, permission_classes
+# from rest_framework_simplejwt.tokens import AccessToken
+# # from .forms import PropertyForm
+# # from .models import Property
+# from .models import Property, Reservation
+# # from .serializers import PropertiesListSerializer
+# from .serializers import PropertiesListSerializer, ReservationsListSerializer
+# from useraccount.models import User
+
+# @api_view(['GET'])
+# @authentication_classes([])
+# @permission_classes([])
+# def properties_list(request):
     #
     # Auth
 
@@ -28,7 +101,7 @@ def properties_list(request):
     #
 
     # favorites = []
-    properties = Property.objects.all()
+    # properties = Property.objects.all()
 
     #
     # Filter
@@ -88,12 +161,12 @@ def properties_list(request):
     #
     #
 
-    serializer = PropertiesListSerializer(properties, many=True)
+    # serializer = PropertiesListSerializer(properties, many=True)
 
-    return JsonResponse({
-        'data': serializer.data,
+    # return JsonResponse({
+    #     'data': serializer.data,
         
-    })
+    # })
 
 
 @api_view(['GET'])
